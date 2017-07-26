bd604a09bad4a68918fd0139058f7e8197b38cb0
Hithroc Mehatoko <contact@hithroc.org>
2017-07-27 00:42:05 +0300

    Store pre-generated image url

--- TelegramFlitter/Card.cs
+++ TelegramFlitter/Card.cs
@@ -5,61 +5,25 @@ using Newtonsoft.Json.Linq;
 namespace TelegramFlitter
 {
 
-    enum CardType
-    {
-        Problem,
-        Mane,
-        Troublemaker,
-        Resource,
-        Event,
-        Friend
-    }
-
     struct Card
     {
-        private static readonly string urlPrefix = "http://ponyhead.com/img/cards/";
-        private static readonly string urlPostfix = ".jpg";
+        enum CardType
+        {
+            Problem,
+            Mane,
+            Troublemaker,
+            Resource,
+            Event,
+            Friend
+        }
+
         public string Title { get; }
         public string SubTitle { get; }
         public string Set { get; }
         public string Number { get; }
         public string FullName { get => Title + (SubTitle == "" ? "" : ", " + SubTitle); }
         public CardType Type { get; }
-        public string ImageUrl
-        {
-            get
-            {
-                var id = Set.ToLower() + Number.ToUpper();
-                // Hardcoded ponyhead mappings
-                if(Set == "PR" && Number == "212")
-                    id =  "prPF1";
-                if(Set == "PR" && Number == "213")
-                    id = "prPF2";
-                if(Set == "CN" && Number == "f2")
-                    id = "cnf2";
-                if(Set == "CN" && Number == "f4")
-                    id = "cnf4";
-                if(Set == "CS" && Number == "f1")
-                    id = "csf1";
-                if(Set == "CG" && Number == "pf11")
-                    id = "cgpf11";
-                if(Set == "CG" && Number == "pf8")
-                    id = "cspf8";
-                if(Set == "AD" && Number == "d1")
-                    id = "add1";
-                if(Set == "AD" && Number == "f10")
-                    id = "adf10";
-                if(Set == "AD" && Number == "f8")
-                    id = "adf8";
-                if(Set == "AD" && Number == "f6")
-                    id = "adf6";
-                if(Set == "AD" && Number == "f12")
-                    id = "adf12";
-                if(Set == "AD" && Number == "f4")
-                    id = "adf4";
-                return urlPrefix + id + urlPostfix;
-            }
-        }
+        public string ImageUrl { get; }
 
         public Card(string title, string subtitle, string set, string number, CardType type)
         {
@@ -68,6 +32,35 @@ namespace TelegramFlitter
             Set = set;
             Number = number;
             Type = type;
+            var id = Set.ToLower() + Number.ToUpper();
+            // Hardcoded ponyhead mappings
+            if(Set == "PR" && Number == "212")
+                id =  "prPF1";
+            if(Set == "PR" && Number == "213")
+                id = "prPF2";
+            if(Set == "CN" && Number == "f2")
+                id = "cnf2";
+            if(Set == "CN" && Number == "f4")
+                id = "cnf4";
+            if(Set == "CS" && Number == "f1")
+                id = "csf1";
+            if(Set == "CG" && Number == "pf11")
+                id = "cgpf11";
+            if(Set == "CG" && Number == "pf8")
+                id = "cspf8";
+            if(Set == "AD" && Number == "d1")
+                id = "add1";
+            if(Set == "AD" && Number == "f10")
+                id = "adf10";
+            if(Set == "AD" && Number == "f8")
+                id = "adf8";
+            if(Set == "AD" && Number == "f6")
+                id = "adf6";
+            if(Set == "AD" && Number == "f12")
+                id = "adf12";
+            if(Set == "AD" && Number == "f4")
+                id = "adf4";
+            ImageUrl = $"http://ponyhead.com/img/{id}.jpg";
         }
 
     }
